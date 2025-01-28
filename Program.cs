a89ae569afff77915073ceeb07b08fcc3224a317
admin <admin@DESKTOP-NRIFEQV>
2025-01-28 18:27:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace cs_project_1
+{
+    /// <summary>
+    /// Класс Program консольного приложения
+    /// </summary>
+    internal class Program
+    {
+
+        /// <summary>
+        /// Метод Main, реализующий функционал консольного приложения
+        /// </summary>
+        internal static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
